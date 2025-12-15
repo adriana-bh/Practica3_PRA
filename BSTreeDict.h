bd529b56543c26fcf3329c7af11d86dbc96be0c5
Adriana Baghdasaryan Hovnatanyan <abaghov@etsinf.upv.es>
2025-12-15 15:34:15 +0100

    Añadida implementación de la clase BSTreeDict.h

--- BSTreeDict.h
+++ BSTreeDict.h
@@ -1,6 +1,7 @@
 #ifndef BSTREEDICT_H
 #define BSTREEDICT_H
 
+#include <iostream>
 #include <ostream>
 #include <stdexcept>
 #include "Dict.h"
@@ -11,27 +12,67 @@ template <typename V>
 class BSTreeDict: public Dict<V> {
 
     private:
-        BSTree<TableEntry<V>>* tree;
+
+        BSTree<TableEntry<V>>* tree;   
 
     public:
+        // constructor
         BSTreeDict(){
-		tree = new BSTree<TableEntry<V>>;		
-	}
-	
-	~BSTreeDict(){
-		delete tree;
-	}
+            tree = new BSTree<TableEntry<V>>;
+        }
 
-	friend std::ostream& operator<<(std::ostream &out, const BSTreeDict<V> &bs){
-		out << *(bs.tree);
-		return out;
-	}
+        // destructor 
+        ~BSTreeDict(){
+            delete tree;
+        }
 
-	V operator[](std::string key){
-		TableEntry<V> valor(key);
-		TableEntry<V> result = tree->search(valor);
-		return result.value;
-	}
+        friend std::ostream& operator<<(std::ostream &out, const BSTreeDict<V> &bs){
+            
+            out << *(bs.tree);  
+            
+            return out;
+        }
+
+
+        V operator[](std::string key){
+
+            TableEntry<V> valor(key);  
+            TableEntry<V> result = tree->search(valor);   
+            
+            return result.value;    
+        }
+
+        // métodos heredados de la interfaz Dict<T>
+
+        void insert(std::string key, V value) override{ 	
+
+            TableEntry<V> valor(key, value);  
+            tree->insert(valor);    
+        } 
+        
+
+        V search(std::string key) override{	    
+            
+            TableEntry<V> valor(key); 
+            TableEntry<V> result = tree->search(valor);
+
+            return result.value;
+        }
+
+
+        V remove(std::string key) override{ 	
+            
+            TableEntry<V> valor(key)
+            TableEntry<V> result = tree->search(valor);
+            tree->remove(valor);
+
+            return result.value;
+        } 
+        
+
+        int entries() override{	            
+            return tree->size();
+        } 
         
 };
 
